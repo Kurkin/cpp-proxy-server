e2d0a588ca29b0b2e61bc057e40dc8ecb4762ae6
Dmitry Kurkin <kurkindmit@gmail.com>
2015-10-31 12:50:30 +0300

    Initial Commit

---
+++ Proxy server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proxy server
+//
+//  Created by Kurkin Dmitry on 31.10.15.
+//  Copyright © 2015 Kurkin Dmitry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
