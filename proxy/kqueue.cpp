83a45b4368f78c3e67c2b1d7bb2802c990cc7076
Dmitry Kurkin <kurkindmit@gmail.com>
2015-12-19 01:32:29 +0300

    timer implement

--- proxy/kqueue.cpp
+++ proxy/kqueue.cpp
@@ -15,8 +15,8 @@
 
 io_queue::io_queue()
 {
-    ident = kqueue();
-    if (ident == -1) {
+    fd = kqueue();
+    if (fd.getfd() == -1) {
         throw_error(errno, "kqueue()");
     }
 }
@@ -32,7 +32,7 @@ void io_queue::add_event_handler(uintptr_t ident, int16_t filter, funct_t funct)
 void io_queue::add_event_handler(uintptr_t ident, int16_t filter, uint16_t flags, funct_t funct) {
     struct kevent event;
     EV_SET(&event, ident, filter, EV_ADD|flags, 0, 0, NULL);
-    int fail = kevent(this->ident, &event, 1, NULL, 0, NULL);
+    int fail = kevent(fd.getfd(), &event, 1, NULL, 0, NULL);
     if (fail) {
         throw_error(errno, "kevent(EV_ADD)");
     }
@@ -45,30 +45,39 @@ void io_queue::add_event_handler(uintptr_t ident, int16_t filter, uint16_t flags
 void io_queue::delete_event_handler(uintptr_t ident, int16_t filter) {
     struct kevent event;
     EV_SET(&event, ident, filter, EV_DELETE, 0, 0, NULL);
-    int fail = kevent(this->ident, &event, 1, NULL, 0, NULL);
+    int fail = kevent(fd.getfd(), &event, 1, NULL, 0, NULL);
     if (fail == -1) {
         if (errno != ENOENT)
             throw_error(errno, "kevent(EV_DELETE)");
     }
-    deleted_idents.push_back({ident, filter});
+    deleted_events.push_back({ident, filter});
     events_handlers.erase(std::pair<uintptr_t, int16_t>(ident, filter));
 }
 
 void io_queue::trigger_user_event_handler(uintptr_t ident) {
     struct kevent event;
     EV_SET(&event, ident, EVFILT_USER, 0, NOTE_TRIGGER, 0, NULL);
-    int fail = kevent(this->ident, &event, 1, NULL, 0, NULL);
+    int fail = kevent(fd.getfd(), &event, 1, NULL, 0, NULL);
     if (fail) {
         throw_error(errno, "kevent(NOTE_TRIGGER)");
     }
+}
+
+timer& io_queue::get_timer() noexcept
+{
+    return timer;
 }
 
 void io_queue::watch_loop() {
     size_t const evListSize = 256;
     struct kevent evList[evListSize];
     
-    while (!finished) {
-        int new_events = kevent(ident, NULL, 0, evList, evListSize, NULL);
+    while (!finished)
+    {
+        int timeout = run_timers_calculate_timeout();
+        struct timespec tmout = {timeout, 0};
+        
+        int new_events = kevent(fd.getfd(), NULL, 0, evList, evListSize, timeout == -1 ? nullptr : &tmout);
         if (new_events == -1) {
             switch (errno) {
                 case EINTR:
@@ -86,20 +95,34 @@ void io_queue::watch_loop() {
                 if (events_handlers.find(event) != events_handlers.end()) {
                     funct_t funct = events_handlers.at(event);
                     funct(evList[i]);
-                    if (deleted_idents.size() != 0) {
+                    if (deleted_events.size() != 0) {
                         for (size_t j = i; j < new_events; j++) {
-                            for (size_t k = 0; k < deleted_idents.size(); k++) {
-                                if (std::make_pair(evList[j].ident, evList[j].filter) == deleted_idents[k]) {
+                            for (size_t k = 0; k < deleted_events.size(); k++) {
+                                if (std::make_pair(evList[j].ident, evList[j].filter) == deleted_events[k]) {
                                     evList[j].ident = -1;
                                 }
                             }
                         }
-                        while (!deleted_idents.empty()) {
-                            deleted_idents.pop_back();
+                        while (!deleted_events.empty()) {
+                            deleted_events.pop_back();
                         }
                     }
                 }
             }
         }
     }
-} 
+}
+
+int io_queue::run_timers_calculate_timeout()
+{
+    if (timer.empty())
+        return -1;
+    
+    timer::clock_t::time_point now = timer::clock_t::now();
+    timer.notify(now);
+    
+    if (timer.empty())
+        return -1;
+    
+    return std::chrono::duration_cast<std::chrono::milliseconds>(timer.top() - now).count();
+}
--- proxy/kqueue.hpp
+++ proxy/kqueue.hpp
@@ -14,13 +14,15 @@
 #include <map>
 #include <vector>
 
+#include "file_descriptor.h"
+#include "timer.h"
+
 typedef std::function<void(struct kevent)> funct_t;
 
 struct io_queue {
     
     io_queue();
     io_queue(io_queue const&) = delete;
-    ~io_queue() { close(ident); }
     
     void add_event_handler(uintptr_t ident, int16_t filter, funct_t funct);
     void add_event_handler(uintptr_t ident, int16_t filter, uint16_t flags, funct_t funct);
@@ -31,11 +33,16 @@ struct io_queue {
     void hard_stop(); //other
     void soft_stop(); //sigterm
     
+    timer& get_timer() noexcept;
+    
 private:
+    int run_timers_calculate_timeout();
+    
     std::map<std::pair<uintptr_t, int16_t>, funct_t> events_handlers;
-    int ident;
-    std::vector<std::pair<uintptr_t, int16_t>> deleted_idents;
+    file_descriptor fd;
+    std::vector<std::pair<uintptr_t, int16_t>> deleted_events;
     bool finished = false;
+    struct timer timer;
 };
 
 #endif /* kqueue_hpp */
